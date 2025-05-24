feb0819604c6b316858ea71adbe08a77c7de709f
Akash Saravanan <akavananuma11@gmail.com>
2025-05-24 17:58:43 +0530

    Time: 1 ms (5.85%), Space: 8.3 MB (28.06%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.c
+++ 3321-type-of-triangle/3321-type-of-triangle.c
@@ -1,3 +1,12 @@
 char* triangleType(int* nums, int numsSize) {
-    
+    if(nums[0]+nums[1]>nums[2] && nums[1]+nums[2]>nums[0] && nums[0]+nums[2]>nums[1])
+    {
+        if(nums[0]==nums[1] && nums[1]==nums[2]&& nums[0]==nums[2])
+        return "equilateral";
+        else if(nums[0]!=nums[1] && nums[1]!=nums[2] && nums[0]!=nums[2])
+        return "scalene";
+        else
+        return "isosceles";
+    }
+    return "none";
 }
