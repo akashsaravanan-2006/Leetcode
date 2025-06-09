65d03ecb3bd862f898ef427c9fb6fd02a9eb1db2
Akash Saravanan <akavananuma11@gmail.com>
2025-06-09 15:29:53 +0530

    Time: 83 ms (6.22%), Space: 8.9 MB (6.81%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -0,0 +1,43 @@
+bool isPalindrome(char* s) {
+    int n,i,k;
+    n=strlen(s);
+    char a[n];
+    char b[n];
+    k=0;
+    for(i=0;i<n;i++)
+    {
+if(s[i] >='a' && s[i]<='z'  || s[i] >='A' && s[i]<='Z' || s[i]>='0' && s[i]<='9' )
+        {
+        if(s[i] >='A' && s[i]<='Z')
+        a[k]=tolower(s[i]);
+        else
+        a[k]=s[i];
+        k++;
+        }
+    }
+    for(i=0;i<k;i++)
+    printf("%c",a[i]);
+    k=0;
+    for(i=n-1;i>=0;i--)
+    {
+        if(s[i] >='a' && s[i]<='z'  || s[i] >='A' && s[i]<='Z' || s[i]>='0' && s[i]<='9' )
+        {
+        if(s[i] >='A' && s[i]<='Z')
+        b[k]=tolower(s[i]);
+        else
+        b[k]=s[i];
+        k++;
+        }
+    }
+    //for(i=0;i<k;i++)
+    //printf("%c",b[i]);
+   for(i=0;i<k;i++)
+   if(a[i]!=b[i])
+   {
+    printf("%c",a[i]);
+    return false;
+   }
+    return true;
+
+
+}
