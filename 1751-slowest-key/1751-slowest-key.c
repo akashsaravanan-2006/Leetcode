cfef916989015ec476dec42b48f8cd0bcc7144b0
Akash Saravanan <akavananuma11@gmail.com>
2025-06-30 23:10:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (52.94%) - LeetHub

--- 1751-slowest-key/1751-slowest-key.c
+++ 1751-slowest-key/1751-slowest-key.c
@@ -1,25 +1,20 @@
 char slowestKey(int* releaseTimes, int releaseTimesSize, char* key) {
-  int i,j,m=0, ans[releaseTimesSize],a[releaseTimesSize],n=0,grt,grts ;
+  int i,j,m=0, ans[releaseTimesSize],a[releaseTimesSize],n=0,grt ;
    ans[m++]=releaseTimes[0];
    for(i=1;i<releaseTimesSize;i++)
    ans[m++]=(releaseTimes[i]-releaseTimes[i-1]);
-
-   for(i=0;i<m;i++)
-   printf("%d\n",ans[i]);
-   
    grt=ans[0];
    for(i=1;i<m;i++)
    if(grt<ans[i])
    grt=ans[i];
-
    for(i=0;i<m;i++)
    if(grt==ans[i])
    a[n++]=i;
    if(n==1)
    return key[a[0]];
-   grts = a[0];
+   grt = a[0];
     for (i = 1; i < n; i++)
-    if (key[a[i]] > key[grts])
-    grts = a[i];
-   return key[grts];
+    if (key[a[i]] > key[grt])
+    grt= a[i];
+   return key[grt];
 }
