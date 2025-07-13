8708e7d83c8a598a21e9825d45122f16810e20e5
Akash Saravanan <akavananuma11@gmail.com>
2025-07-13 13:10:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.93%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.c
+++ 3636-check-balanced-string/3636-check-balanced-string.c
@@ -1,12 +1,14 @@
 bool isBalanced(char* num) {
-    int a[100],s=0,s1=0,i,m=0,n;
+    int s=0,s1=0,i,n;
     n=strlen(num);
     for(i=0;i<n;i++)
-    a[m++]=num[i]-'0';
-    for(i=0;i<m;i=i+2)
-    s=s+a[i];
-    for(i=1;i<m;i=i+2)
-    s1=s1+a[i];
+    {
+        if(i%2==0)
+        s=s+(num[i]-'0');
+        else
+        s1=s1+(num[i]-'0');
+
+    }
     if(s==s1)
     return true;
     return false;
