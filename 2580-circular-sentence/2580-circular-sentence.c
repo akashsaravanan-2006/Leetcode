6261bfc0910d6a126e3d6eda3efb6496d6ba703d
Akash Saravanan <akavananuma11@gmail.com>
2025-07-18 13:46:20 +0530

    Time: 0 ms (100%), Space: 8.1 MB (68.18%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.c
+++ 2580-circular-sentence/2580-circular-sentence.c
@@ -1,24 +1,10 @@
 bool isCircularSentence(char* sentence) {
     int i,n,f=0;
     n=strlen(sentence);
-    for(i=0;i<n;i++)
-    if(sentence[i]==' ')
-    {
-        f=1;
-        break;
-    }
-    if(f==1)
-    {
    for(i=0;i<n;i++) 
    if(sentence[i]==' ' && sentence[i-1] !=sentence[i+1])
    return false;
-    }
-    if(f==0)
-    {
-    if(sentence[0]!=sentence[n-1])
-    return false;
-    }
-    if(sentence[0]==sentence[n-1])
+   if(sentence[0]==sentence[n-1])
     return true;
     return false;
 }
