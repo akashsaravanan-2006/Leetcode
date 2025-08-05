6a39778d60560239d856ae1359cc9830be86a738
Akash Saravanan <akavananuma11@gmail.com>
2025-08-05 09:49:55 +0530

    Time: 3 ms (94.59%), Space: 9.4 MB (78.38%) - LeetHub

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.c
@@ -0,0 +1,25 @@
+int minOperations(int* nums, int numsSize, int k) {
+    int i,j,f,m=0,c=0;
+    for(i=0;i<numsSize;i++)
+    {
+        f=1;
+        for(j=0;j<i;j++)
+        if(nums[i]==nums[j])
+        {
+            f=0;
+            break;
+        }
+        if(f==1)
+        nums[m++]=nums[i];
+    }
+    if(m==1 && k==nums[0])
+    return 0;
+    for(i=0;i<m;i++)
+    {
+    if(nums[i]>k)
+    c++;
+    if(nums[i]<k && nums[i]!=k)
+    return -1;
+    }
+    return c;
+}
