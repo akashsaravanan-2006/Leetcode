ea6eed7b7b9a6b76c5329284a10e92ffdf4628ab
Akash Saravanan <akavananuma11@gmail.com>
2025-08-19 20:35:33 +0530

    Time: 254 ms (52.78%), Space: 9.5 MB (27.78%) - LeetHub

---
+++ 2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.c
@@ -0,0 +1,25 @@
+int maxDivScore(int* nums, int numsSize, int* divisors, int divisorsSize) {
+    int i,j,c,grt=0,grti=divisors[0];
+   for(i=0;i<divisorsSize;i++)
+   {
+    c=0;
+   for(j=0;j<numsSize;j++)
+   {
+   if(nums[j]%divisors[i]==0)
+   c++;
+   }
+   if(grt<c)
+   {
+   grt=c;
+   grti=divisors[i];
+   }
+   else if(grt==c && grti>divisors[i])
+   {
+   grt=c;
+   grti=divisors[i];
+   }
+   }
+   return grti;
+
+
+}
