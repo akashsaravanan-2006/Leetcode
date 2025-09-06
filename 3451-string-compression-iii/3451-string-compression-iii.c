a41273cc899e503a76b7dba3d55b2b1c1f096a2d
Akash Saravanan <akavananuma11@gmail.com>
2025-09-06 08:07:39 +0530

    Time: 10 ms (41.67%), Space: 26.5 MB (16.67%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.c
@@ -0,0 +1,38 @@
+char* compressedString(char* word) {
+    int i,c=0,m=0,n;
+    char*a=(char*)malloc(20000000*sizeof(char));
+    n=strlen(word);
+for(i=0;i<n;i++)
+{
+    c++;
+if(word[i]!=word[i+1])
+{
+       if(c<=9)
+       {
+       a[m++]=c+'0';
+       a[m++]=word[i];
+       }
+       else
+       {
+          while(c!=0)
+          {
+            if(c>9)
+            {
+            c=c-9;
+            a[m++]='9';
+            a[m++]=word[i];
+            }
+            else
+            {
+            a[m++]=c+'0';
+            a[m++]=word[i];
+            c=0;
+            }
+          }
+       }
+       c=0;
+}
+}
+a[m]='\0';
+return a;
+}
