aa56ce2983c0774071975186cf3570cb0dd319b5
Akash Saravanan <akavananuma11@gmail.com>
2025-12-31 12:08:37 +0530

    Time: 3 ms (3.36%), Space: 42.7 MB (5.16%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        String n1=Integer.toBinaryString(start);
+        String n2=Integer.toBinaryString(goal);
+        int x=Math.max(n1.length(), n2.length());
+        while(n1.length()<x) 
+        n1="0"+n1;
+        while(n2.length()<x)
+        n2="0"+n2;
+        int i=0,c=0;
+        while(i<x) 
+        {
+            if(n1.charAt(i)!=n2.charAt(i))
+            c++;
+            i++;
+        }
+
+        return c;
+    }
+}
