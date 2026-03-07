24ec7ebd9a22f3e611287b998f2dd57ddd29e5b2
Akash Saravanan <akavananuma11@gmail.com>
2026-03-07 20:51:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,6 +1,13 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-     int*ans=(int*)malloc(2*sizeof(int));
-}
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[]=new int[2];
+        for(int i=0;i<nums.length;i++)
+        for(int j=i+1;j<nums.length;j++)
+        if(nums[i]+nums[j]==target)
+        {
+            ans[0]=i;
+            ans[1]=j;
+            break;
+        }
+        return ans;
+    }
